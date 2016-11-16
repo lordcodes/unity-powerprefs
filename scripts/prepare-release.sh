ff5ba6adac3cb18e579964d4502449cd4e5392e5
Andrew Lord <andrew@lordcodes.com>
2016-11-16 22:20:40 +0000

    Added script to export a package of files and added it to the Travis script. Added a script to prepare a release (copy the unity package to release directory and add specified version number to the name of the package).

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+version=$1
+
+project="PowerPrefs"
+package_path=$(pwd)/current-package/$project.unitypackage
+release_directory=$(pwd)/release
+release_path=$release_directory/$project.$version.unitypackage
+
+mkdir -p $release_directory
+
+echo "Preparing release version $version."
+cp "$package_path" "$release_path"
