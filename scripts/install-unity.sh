02121e4020f498ed18002e631f36f7917bc696e5
Andrew Lord <andrew@lordcodes.com>
2016-11-16 21:00:27 +0000

    Added scripts to build the project into Mac OS app and to run unit tests. Removed gradle scripts. Cleaned up root gitignore.

---
+++ scripts/install-unity.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=649f48bbbf0f
+VERSION=5.4.1f1
+
+download() {
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
---
+++ scripts/run-tests.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+project="ci-build"
+project_path=$(pwd)/src/PowerPrefs
+log_file=$(pwd)/build/unity-mac.log
+test_results=$(pwd)/reports/unit-test-results.xml
+
+error_code=0
+
+echo "Running unit tests."
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile "$log_file" \
+  -projectPath "$project_path" \
+  -editorTestsResultFile "$test_results" \
+  -runEditorTests \
+  -quit
+
+if [ $? = 0 ] ; then
+  echo "Unit tests all passed successfully."
+  error_code=0
+else
+  echo "Unit tests failed. Exited with $?."
+  error_code=1
+fi
+
+echo 'Build logs:'
+cat $log_file
+
+echo 'Test results:'
+cat $test_results
+
+echo "Finishing with code $error_code"
+exit $error_code
