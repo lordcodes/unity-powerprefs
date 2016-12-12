1058a3675a5ba82a236b943d854cabdda07cbcc9
Andrew Lord <andrew@lordcodes.com>
2016-12-12 23:09:03 +0000

    Added updated export package script.

--- scripts/export-package.sh
+++ scripts/export-package.sh
@@ -17,11 +17,19 @@
 # limitations under the License.
 ################################################################################
 
-project=$1
+company=$1
+project=$2
+package=$3
 project_path=$(pwd)/src/$project
 log_file=$(pwd)/build/unity-mac.log
 export_directory=$(pwd)/current-package
-export_path=$export_directory/$project.unitypackage
+if [ -z "$package" ]; then
+  export_path=$export_directory/$project.unitypackage
+  asset_path="Assets/$company/$project"
+else
+  export_path=$export_directory/$project.$package.unitypackage
+  asset_path="Assets/$company/$project/$package"
+fi
 error_code=0
 
 mkdir -p $export_directory
@@ -33,7 +41,7 @@ echo "Creating package."
   -silent-crashes \
   -logFile "$log_file" \
   -projectPath "$project_path" \
-  -exportPackage "Assets/$project" "$export_path" \
+  -exportPackage "$asset_path" "$export_path" \
   -quit
 if [ $? = 0 ] ; then
   echo "Created package successfully."
