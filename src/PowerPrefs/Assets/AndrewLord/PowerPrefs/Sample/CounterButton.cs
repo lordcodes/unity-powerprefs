6d4b4d4f8be6fe2a14137e1da724891a05f6498b
Andrew Lord <andrew@lordcodes.com>
2017-04-06 23:03:55 +0100

    Add sample scene (#22)
    * Renamed PowerPrefsValue to PowerPrefsKeyAccessor.
    
    * Renamed “Value” method in PowerPrefsAccessor to “KeyAccessor”. Fixed tests etc.
    
    * Renamed PowerPrefsValue to PowerPrefsKeyAccessor in the README.
    
    * Added retry function to the install step of travis script.
    
    * Moved existing code into “Core” folder. Added a sample scene into “Sample” folder.

---
+++ src/PowerPrefs/Assets/AndrewLord/PowerPrefs/Sample/CounterButton.cs
@@ -0,0 +1,45 @@
+//
+// Copyright (C) 2017 Andrew Lord
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License.
+//
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and limitations under the License.
+//
+namespace AndrewLord.UnityPowerPrefsSample {
+
+  using AndrewLord.UnityPowerPrefs;
+  using UnityEngine;
+  using UnityEngine.UI;
+
+  public class CounterButton : MonoBehaviour {
+
+    private const string KeyCounter = "KeyCounter";
+
+    public Text label;
+
+    private PowerPrefsKeyAccessor<int> accessor;
+
+    void Awake() {
+      accessor = PowerPrefs.ForInt().KeyAccessor(KeyCounter);
+      CounterChanged(accessor.Get());
+    }
+
+    public void IncrementCounter() {
+      int counter = accessor.Get() + 1;
+      accessor.Set(counter);
+      CounterChanged(counter);
+    }
+
+    private void CounterChanged(int counter) {
+      label.text = counter.ToString();
+    }
+  }
+}
